d6b48fc79a7e7d1da28540c85d7bac0284cb98a9
Stavros Avramidis <stavros9899@gmail.com>
2025-08-03 10:26:49 +0300

    Adds 1 M-cycle delay to DMA operation
    Signed-off-by: Stavros Avramidis <stavros9899@gmail.com>

--- dmg/src/memory/dma.rs
+++ dmg/src/memory/dma.rs
@@ -6,9 +6,10 @@ use std::rc::Weak;
 
 #[derive(Debug)]
 pub struct DMA {
-    source: u16,
+    source: u8,
+    next_addr: u16,
     enabled: bool,
-    offset: u8,
+    starting: bool,
     pub mmu: Weak<RefCell<MMU>>,
 }
 
@@ -16,28 +17,33 @@ impl DMA {
     pub fn new(mmu: Weak<RefCell<MMU>>) -> Self {
         DMA {
             source: 0,
+            next_addr: 0,
+            starting: false,
             enabled: false,
-            offset: 0,
             mmu,
         }
     }
 
     pub fn tick(&mut self) {
-        if !self.enabled {
-            return;
+        if self.enabled {
+            let mmu_rc = self.mmu.upgrade().unwrap();
+            let src_addr = self.next_addr;
+            let dst_addr = 0xFE00 | (self.next_addr & 0xFF);
+
+            let value = mmu_rc.borrow().read(src_addr);
+            mmu_rc.borrow_mut().write(dst_addr, value);
+
+            self.next_addr += 1;
+
+            if src_addr as u8 > 0x9F {
+                self.enabled = false;
+            }
         }
 
-        let mmu_rc = self.mmu.upgrade().unwrap();
-        let src_addr = self.source | self.offset as u16;
-        let dst_addr = 0xFE00 | self.offset as u16;
-
-        let value = mmu_rc.borrow().read(src_addr);
-        mmu_rc.borrow_mut().write(dst_addr, value);
-
-        self.offset += 1;
-
-        if self.offset > 0x9F {
-            self.enabled = false;
+        if self.starting {
+            self.enabled = true;
+            self.starting = false;
+            self.next_addr = (self.source as u16) << 8;
         }
     }
 
@@ -48,12 +54,12 @@ impl DMA {
 
 impl RegisterTrait for DMA {
     fn read(&self, _: u16) -> u8 {
-        return (self.source >> 8) as u8;
+        return self.source;
     }
 
     fn write(&mut self, _: u16, value: u8) {
-        self.source = (value as u16) << 8;
-        self.offset = 0;
-        self.enabled = true;
+        self.source = value;
+        self.next_addr = 0;
+        self.starting = true;
     }
 }
